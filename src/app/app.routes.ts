831d2b14f1c36701c3da5f1fa08510f81c8b0097
Demetre <136166143+DemetreD@users.noreply.github.com>
2025-11-10 06:47:56 -0800

    Add posts route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'users',
     component: UsersComponent,
+  },
+  {
+    path: 'posts',
+    component: PostsComponent,
   },
 ];
