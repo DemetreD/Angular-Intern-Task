d42b605f3e995b861e4931ad54028fed9e80498f
Demetre <136166143+DemetreD@users.noreply.github.com>
2025-11-10 06:26:50 -0800

    Show users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, NavigationComponent],
+  imports: [RouterOutlet, HeaderComponent, NavigationComponent, UsersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
